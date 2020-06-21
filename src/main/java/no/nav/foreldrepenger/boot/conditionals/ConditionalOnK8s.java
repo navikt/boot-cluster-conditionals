43be0f6f69a6e2cc2f744af137335f9aef2daaec
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-21 10:57:54 +0200

    test

--- src/main/java/no/nav/foreldrepenger/boot/conditionals/ConditionalOnK8s.java
+++ src/main/java/no/nav/foreldrepenger/boot/conditionals/ConditionalOnK8s.java
@@ -6,12 +6,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Conditional(OnK8sCondition.class)
+@Bean
 public @interface ConditionalOnK8s {
 
 }
