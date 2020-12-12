b9ac1882a2119df320dc6bdef82ab75fb5192dc4
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-12-12 11:42:59 +0100

    constant

--- src/main/java/no/nav/foreldrepenger/boot/conditionals/EnvUtil.java
+++ src/main/java/no/nav/foreldrepenger/boot/conditionals/EnvUtil.java
@@ -9,19 +9,22 @@ public final class EnvUtil {
 
     public static final Marker CONFIDENTIAL = MarkerFactory.getMarker("CONFIDENTIAL");
 
-    public static final String TEST = "test";
-    public static final String DEV = "dev";
-    public static final String PROD = "prod";
-    public static final String DEV_GCP = DEV + "-gcp";
-    public static final String PROD_GCP = PROD + "-gcp";
-
+    private static final String FSS = "-fss";
+    private static final String SBS = "-sbs";
+    private static final String GCP = "-gcp";
     public static final String LOCAL = "local";
     public static final String DEFAULT = "default";
 
-    public static final String PROD_SBS = PROD + "-sbs";
-    public static final String DEV_SBS = DEV + "-sbs";
-    public static final String PROD_FSS = PROD + "-fss";
-    public static final String DEV_FSS = DEV + "-fss";
+    public static final String TEST = "test";
+    public static final String DEV = "dev";
+    public static final String PROD = "prod";
+    public static final String DEV_GCP = DEV + GCP;
+    public static final String PROD_GCP = PROD + GCP;
+
+    public static final String PROD_SBS = PROD + SBS;
+    public static final String DEV_SBS = DEV + SBS;
+    public static final String PROD_FSS = PROD + FSS;
+    public static final String DEV_FSS = DEV + FSS;
 
     private EnvUtil() {
 
